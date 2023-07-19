9f3e8c20d9306017ba681ab0f8df88314f894641
Tamer Akdeniz <tamer.akdeniz3@gmail.com>
2023-07-19 19:37:38 +0300

    Python ile temel alıştırmalar

---
+++ deneme.py
@@ -0,0 +1,43 @@
+print("Python ile Yapay Zekâ Uygulamaları") 
+print('\'Python\' ile Yapay Zekâ Uygulamaları') 
+print("Python programlama dilinin adı \"piton\" yılanından gelmez.") 
+print('Python programlama dilinin adı "piton" yılanından gelmez.') 
+print("Python programlama dilinin adı 'piton' yılanından gelmez.")
+
+dil = "Python" 
+konu = "Yapay Zeka Uygulamaları" 
+print(dil+" ile "+konu)
+
+print("w"*3+"."+"cu.edu.tr")
+
+print('Python ile programlamaya giriş.') 
+print("Python ile programlamaya giriş.") 
+print("""Python ile programlamaya giriş.""")
+
+print('Pyhton programlama dilinin "piton" yılanı ile ilgisi yoktur.') 
+print("Pyhton programlama dilinin 'piton' yılanı ile ilgisi yoktur.")
+
+hakkinda = """Python programlama dili, Guido Van Rossum adlı Hollandalı bir programcı tarafından 90’lı yılların başında geliştirilmeye başlanmıştır. Çoğu insan, isminin "Python" olmasına bakarak, bu programlama dilinin adını piton yılanından aldığını düşünür. Ancak zannedildiğinin aksine bu programlama dilinin adı piton yılanından gelmez.""" 
+print(hakkinda)
+
+print(type(33)) 
+print(type("Adana")) 
+print("Elma kelimesindeki harf sayısı..:",len("Elma"))
+
+print("Tamer","Akdeniz","Isparta",2002)
+# Tamer Akdeniz Isparta 2002
+print("Mustafa","Kemal","Atatürk",sep=" ") 
+print("Adana","Mersin",sep=None)
+#Mustafa Kemal Atatürk #Adana Mersin
+print("Adana","Mersin","Antalya",sep="->") # Adana->Mersin->Antalya 
+print(10,11,1938,sep="/")
+# 10/11/1938
+
+print("Yapay", "Zekâ", "Uygulamaları",sep="\n",end=".")
+#print(dir(str)) #string veriler üzerinde işlem yapan fonksiyonları listeler 
+#print(dir(int)) #integer veriler üzerinde işlem yapan fonksiyonları listeler 
+#print(dir(float)) #float veriler üzerinde işlem yapan fonksiyonları listeler 
+#print(dir(complex)) #complex veriler üzerinde işlem yapan fonksiyonları listeler
+
+dizi="ADANA BÜYÜKŞEHİR BELEDİYESİ" 
+print(dizi.split())
